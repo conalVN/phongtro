e7938e537af4034d974b2bc5c5c551c72c7d6b7e
conal <develooper2022@gmail.com>
2023-05-31 14:10:07 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -18,6 +18,9 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 initRoutes(app);
+app.use("", (req, res) => {
+  res.json("server on");
+});
 connectDatabase();
 
 const port = process.env.PORT || 8888;
